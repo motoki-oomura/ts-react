853e81fc853a1279c39ad422b6092a8c623fb5f3
Motoki Oomura <m.omura@leihauoli.com>
2022-08-24 12:04:23 +0900

    hookを追加 useScroll

---
+++ src/hooks/useScroll.ts
@@ -0,0 +1,21 @@
+import {useEffect, useState} from "react";
+
+
+const useScroll = (element: unknown, options: IntersectionObserverInit = {}) => {
+    const [isIntersecting, set] = useState(false);
+
+    useEffect(() => {
+        if (!(element instanceof Element)) return;
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => set(entry.isIntersecting));
+        }, options);
+
+        observer.observe(element);
+        return (): void => element && observer.unobserve(element);
+    }, [element]);
+
+    return isIntersecting;
+};
+
+export default useScroll;
