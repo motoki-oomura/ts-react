6ddbe29dd73e0f4c8a15b3317fc9a407b96ce315
Motoki Oomura <m.omura@leihauoli.com>
2022-09-22 16:41:16 +0900

    ビルド環境をviteに移行

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    server: {
+        open: true,
+        hmr: {
+            host: 'localhost'
+        },
+    },
+    plugins: [react(), tsconfigPaths()]
+})
