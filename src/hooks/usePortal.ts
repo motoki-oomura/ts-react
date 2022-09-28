da11e3ae3f181b1f0095902a1a324ad11dac56c8
Motoki Oomura <m.omura@leihauoli.com>
2022-09-28 17:33:47 +0900

    usePortalの作成

---
+++ src/hooks/usePortal.ts
@@ -0,0 +1,40 @@
+import React, {useCallback, useEffect, useRef, useState} from "react";
+import ReactDOM from "react-dom";
+
+/**
+ * Portalを簡単に実装するためのフック
+ */
+type Options = Partial<{
+    initView: boolean; // Mount時に表示するか
+}>;
+const usePortal = (id = 'unknown', options: Options = {}) => {
+    const { initView } = options;
+    const [isOpen, setIsOpen] = useState(false);
+    const ref = useRef<HTMLDivElement | null>(null);
+
+    const showPortal = useCallback(() => setIsOpen(true), []);
+    const hiddenPortal = useCallback(() => setIsOpen(false), []);
+
+    const renderPortal = useCallback((children: React.ReactNode) => {
+        if (!isOpen) return null;
+        return ref.current && ReactDOM.createPortal(children, ref.current);
+    }, [isOpen]);
+
+    useEffect(() => {
+        if (ref.current !== null) return;
+        const el = document.createElement('div');
+        el.setAttribute('id', `portal_${id}`);
+        document.body.appendChild(el);
+        ref.current = el;
+
+        if (initView) setIsOpen(true);
+
+        return () => {
+            ref.current = null;
+            document.body.removeChild(el);
+        }
+    }, [id, initView]);
+
+    return { renderPortal, showPortal, hiddenPortal };
+};
+export default usePortal;
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,8 @@ import {setRoutes, setTypeRoutes} from "@features/Router";
 import IndexPage from "@pages/IndexPage";
 
 // demo
-import DemoInfiniteScroll from "@pages/_demo/InfiniteScroll";
+import DemoInfiniteScroll from "@pages/_demo/DemoInfiniteScroll";
+import DemoPortal from "@pages/_demo/DemoPortal";
 
 setRoutes(
     [
@@ -18,5 +19,9 @@ setTypeRoutes('_demo', [
     {
         path: '/_demo/infinite-scroll',
         element: <><DemoInfiniteScroll /></>
+    },
+    {
+        path: '/_demo/portal',
+        element: <><DemoPortal /></>
     }
 ]);
