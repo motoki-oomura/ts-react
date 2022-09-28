b67aa0cd30602d7ac344fc356ec8a4efaa25b6d0
Motoki Oomura <m.omura@leihauoli.com>
2022-09-28 18:00:43 +0900

    usePrevState Hookを追加

---
+++ src/hooks/usePrevState.ts
@@ -0,0 +1,12 @@
+import {useEffect, useRef} from "react";
+
+const usePrevState = <T>(value: T) => {
+    const ref = useRef<T | undefined>();
+
+    useEffect(() => {
+        ref.current = value;
+    }, [value]);
+
+    return ref.current;
+};
+export default usePrevState;
