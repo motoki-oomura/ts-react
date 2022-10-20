bfb41d8f4864ced430335bc71421d353bb297e82
Motoki Oomura <m.omura@leihauoli.com>
2022-10-20 15:56:24 +0900

    envの変数をindex.htmlで使用できるように改修

--- src/pages/IndexPage/index.tsx
+++ src/pages/IndexPage/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { DEMO_FORM_SIGNUP } from '../../routes';
+import {MODE} from "@constants/env";
 
 const IndexPage = () => {
     const navigate = useNavigate();
-    return <div onClick={() => navigate(DEMO_FORM_SIGNUP.path)}>index page</div>;
+    return <div onClick={() => navigate(DEMO_FORM_SIGNUP.path)}>index page. mode: { MODE }</div>;
 };
 export default IndexPage;
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    server: {
-        open: true,
-        hmr: {
-            host: 'localhost'
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.');
+    return {
+        server: {
+            open: true,
+            hmr: {
+                host: 'localhost'
+            },
         },
+        resolve: {},
+        plugins: [react(), tsconfigPaths(), htmlPlugin(env)]
+    }
+});
+
+const htmlPlugin = (env: ReturnType<typeof loadEnv>) => ({
+    name: "html-transform",
+    transformIndexHtml: {
+        enforce: "pre" as const,
+        transform: (html: string): string =>
+            html.replace(/%(.*?)%/g, (match, p1) => env[p1] ?? match),
     },
-    resolve: {},
-    plugins: [react(), tsconfigPaths()]
-})
+});
