abac0c92075600225a4bf76b4abf31edbcb0566a
motoki.oomura <motoki.oomura@gmail.com>
2023-10-25 18:12:27 +0900

    Feature/utils object (#4)
    * create object utils.
    * add utils/object

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -5,3 +5,11 @@ export const isObject = (value: unknown): value is object => {
         && typeof value === 'object'
         && !Array.isArray(value);
 };
+
+/**
+ * 配列データを指定したkey名をオブジェクトのkeyとしてもつデータへ変換
+ * example. [{ id: 1, name: 'hoge' }] => { "1": { id: 1, name: 'hoge' } }
+ */
+export const convertObjectData = <T extends object>(arr: T[], uniqueValueKey: keyof T): { [key: string]: T } => {
+    return Object.fromEntries(arr.map((d) => [d[uniqueValueKey], d]));
+};
